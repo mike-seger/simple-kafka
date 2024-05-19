3289284f66871dec0c6a98189ca85e703acf1679
Michael Seger <michael@net128.com>
2024-05-19 19:00:31 +0200

    ...

--- src/main/java/com/net128/tool/generic/avro/client/controllers/AvroController.java
+++ src/main/java/com/net128/tool/generic/avro/client/controllers/AvroController.java
@@ -36,7 +36,7 @@ public class AvroController {
         schemaRegistryService.addSchema(name, schema);
     }
 
-    @PostMapping(value = "/schema/{name}/serialize/", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @PostMapping(value = "/schema/{name}/serialize", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @Operation(summary = "Serialize JSON to Avro",
         requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
             content = @Content(schema = @Schema(implementation = Object.class),
@@ -47,15 +47,14 @@ public class AvroController {
         return avroUtils.serializeToAvro(name, json);
     }
 
-    @PostMapping(value = "/schema/{name}/hexdump/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "/schema/{name}/hexdump", produces = MediaType.TEXT_PLAIN_VALUE)
     @Operation(summary = "Serialize JSON to Avro Hex Dump",
         requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
             content = @Content(schema = @Schema(implementation = Object.class),
                 examples = { @ExampleObject(value = exampleObject) })),
         parameters = {@Parameter(name = "name", description = "Name of the schema / topic",
             example = "user1", schema = @Schema(type = "string"))})
-    @ApiResponse(responseCode = "200", content = @Content(examples = { @ExampleObject(
-        value = "0000  08 4A 6F 65 32 80 7D                             |.Joe2.}|")}))
+    @ApiResponse(responseCode = "200", content = @Content(examples = { @ExampleObject(value = hexDump)}))
     public String toAvroHexDump(@RequestBody String json, @PathVariable String name) throws Exception {
         return avroUtils.jsonToAvroHexDump(name, json);
     }
@@ -63,8 +62,19 @@ public class AvroController {
     final static String exampleObject =
         """
         {
-            "name": "Joe2",
-            "age": 8000
+            "id": "dff964a6-7f97-49da-8eb5-108701e1e3b3",
+            "name": "Maria do Carmo Mão de Ferro e Cunha de Almeida Santa Rita Santos Abreu",
+            "age": 33
         }
         """;
+
+    final static String hexDump = """
+            0000  64 66 66 39 36 34 61 36 2D 37 66 39 37 2D 34 39  |dff964a6-7f97-49|
+            0010  64 61 2D 38 65 62 35 2D 31 30 38 37 30 31 65 31  |da-8eb5-108701e1|
+            0020  65 33 62 33 8E 01 4D 61 72 69 61 20 64 6F 20 43  |e3b3..Maria do C|
+            0030  61 72 6D 6F 20 4D C3 A3 6F 20 64 65 20 46 65 72  |armo M..o de Fer|
+            0040  72 6F 20 65 20 43 75 6E 68 61 20 64 65 20 41 6C  |ro e Cunha de Al|
+            0050  6D 65 69 64 61 20 53 61 6E 74 61 20 52 69 74 61  |meida Santa Rita|
+            0060  20 53 61 6E 74 6F 73 20 41 62 72 65 75 42        | Santos AbreuB|
+            """;
 }
