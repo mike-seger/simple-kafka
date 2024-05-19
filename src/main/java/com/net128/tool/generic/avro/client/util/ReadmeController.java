a20e2df1f6097b66c4cd4fbf8b303a3a708d4035
Michael Seger <michael@net128.com>
2024-05-19 12:47:32 +0200

    ...

--- src/main/java/com/net128/tool/generic/avro/client/util/ReadmeController.java
+++ src/main/java/com/net128/tool/generic/avro/client/util/ReadmeController.java
@@ -4,34 +4,36 @@ package com.net128.tool.generic.avro.client.util;
 
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.Options;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.net.URISyntaxException;
 
 @Controller
 public class ReadmeController {
+    private String indexPage = null;
 
     @GetMapping(value = {"/", "/index.html"}, produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
-    public String getReadmeAsHtml() throws IOException {
-        ClassPathResource resource = new ClassPathResource("static/README.adoc");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
-            String asciidoc = reader.lines().collect(Collectors.joining("\n"));
-
-                asciidoc = asciidoc.replace("http://localhost:8080", "");
-
-            try (Asciidoctor asciidoctor = Asciidoctor.Factory.create()) {
-                Options options = Options.builder().toFile(false).build();
-                return asciidoctor.convert(asciidoc, options);
-            }
+    public String getReadmeAsHtml() throws IOException, URISyntaxException {
+        if(indexPage != null) return indexPage;
+        var asciidoc = ResourceUtil.loadResourceFromLocation("static/README.adoc");
+        asciidoc = asciidoc.replaceAll("([^ :])http://localhost:8080/([^ ]*)", "$1 link:$2");
+        var htmlContent = "";
+        try (var asciidoctor = Asciidoctor.Factory.create()) {
+            var options = Options.builder()
+                    .backend("html")
+                    .safe(org.asciidoctor.SafeMode.UNSAFE)
+                    .build();
+            //var options = Options.builder().toFile(false).build();
+            htmlContent = asciidoctor.convert(asciidoc, options);
         }
+
+        var indexTemplate = ResourceUtil.loadResourceFromLocation("static/index-template.html");
+        indexPage = indexTemplate.replace("${content}", htmlContent);
+        return indexPage;
     }
 }
